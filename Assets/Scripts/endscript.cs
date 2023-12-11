b10ced4ea2bd4ff5ad0b4551a4bdfa64cc11e651
N1KO-G <146829154+N1KO-G@users.noreply.github.com>
2023-12-11 16:05:09 +0000

    upd

--- Assets/Scripts/endscript.cs
+++ Assets/Scripts/endscript.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class deathscript : MonoBehaviour
+public class endscript : MonoBehaviour
 {
-   void OnTriggerEnter( Collider col)
+    void OnTriggerEnter(Collider col)
     {
-        if(col.CompareTag("Player"))
+        if (col.CompareTag("Player"))
         {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene(4);
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
     }
-
-   
-
-   
 }
